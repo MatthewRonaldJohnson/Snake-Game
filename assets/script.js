34bc24479942312ec5b72252d24857ffd8126911
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-03-13 16:11:37 -0600

    working on adding length to snake, non functional

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,10 @@ var xPosition = 300;
 var pXpos;
 var pYpos;
 var pellet = document.createElement('div');
-var segments = {}
+var segments = []
 var segmentPositions = [];
 
-document.addEventListener("keydown", function (e) { 
+document.addEventListener("keydown", function (e) {
     switch (e.key) {
         case 'ArrowUp': direction = 'up';
             break;
@@ -27,6 +27,7 @@ document.addEventListener("keydown", function (e) {
 function playGame() {
     makePellet();
     var gamePlayLoop = setInterval(function () {
+        moveSegments();
         switch (direction) {
             case 'up':
                 yPosition -= 4;
@@ -45,37 +46,47 @@ function playGame() {
                 player.style.right = `${xPosition}px`
                 break;
         }
-        if((yPosition <= (pYpos+10) && yPosition >= (pYpos-10)) && (xPosition <= (pXpos+10) && xPosition >= (pXpos-10))){
+        if ((yPosition <= (pYpos + 10) && yPosition >= (pYpos - 10)) && (xPosition <= (pXpos + 10) && xPosition >= (pXpos - 10))) {
             score++;
             playingArea.removeChild(pellet);
             makePellet();
             upDateScore();
             growSnake();
         }
-        if(yPosition <= -5 || yPosition >= 595 || xPosition <= -5 || xPosition >= 595){
+        if (yPosition <= -5 || yPosition >= 595 || xPosition <= -5 || xPosition >= 595) {
             gameOver();
             clearInterval(gamePlayLoop);
         }
-    }, 17);
+    }, 50);
 }
 
-function makePellet(){
-    pXpos = Math.floor(Math.random()*601);
-    pYpos = Math.floor(Math.random()*601);
+function moveSegments(){
+    segmentPositions = segmentPositions.splice(0, segmentPositions.length-1) //remove last item in array
+    segmentPositions.unshift([xPosition,yPosition]) //add current player location to index 0 of array
+    for (let i = 0; i < segments.length; i++) {
+        segments[i].style.top = segmentPositions[i][0];
+        segments[i].style.right = segmentPositions[i][1];
+    }
+}
+
+function makePellet() {
+    pXpos = Math.floor(Math.random() * (595 - 5 + 1) + 5);
+    pYpos = Math.floor(Math.random() * (595 - 5 + 1) + 5);
     pellet.style = `width: 10px; height: 10px; background-color: green; position: absolute; right: ${pXpos}px; top: ${pYpos}px;`
     playingArea.appendChild(pellet);
 }
 
-function upDateScore(){
+function upDateScore() {
     scoreDisplay.textContent = `Score: ${score}`
 }
 
-function gameOver(){
+function gameOver() {
     console.log('you lose')
 }
 
-function growSnake(){
-    segments[score] = document.createElement('div')
-    segments[score].style = `width: 10px; height: 10px; background-color: grey; position: absolute; right: ${xPosition}px; top: ${yPosition}px;`
-    playingArea.appendChild(segments[score])
+function growSnake() {
+    segments.push(document.createElement('div'))
+    segmentPositions.push([xPosition, yPosition])
+    segments[score - 1].style = `width: 10px; height: 10px; background-color: grey; position: absolute; right: ${segmentPositions[score - 1][0]}px; top: ${segmentPositions[score - 1][1]}px;`
+    playingArea.appendChild(segments[score - 1])
 }
